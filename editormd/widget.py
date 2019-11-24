595d42f7ed3b6ed2cb246695984aa7d5d80a9e30
ngdyj <sadeye@qq.com>
2019-11-24 21:22:42 +0800

    fix:后台markdown不支持'-'语法的错误

--- editormd/widget.py
+++ editormd/widget.py
@@ -1,36 +1,26 @@
 from django.forms import widgets
 from django.utils.safestring import mark_safe
-from django.utils.html import conditional_escape
-from django.utils.encoding import force_text
+from django.template.loader import render_to_string
 
 
 class TextInputMarkdown(widgets.Textarea):
     """
-    please see: https://github.com/pandao/editor.md
+    please see:
+        https://github.com/pandao/editor.md
+        https://github.com/pylixm/django-mdeditor
     """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.lib_path = '//cdn.jsdelivr.net/gh/pandao/editor.md@1.5.0/lib/'
+
     def render(self, name, value, attrs=None, renderer=None):
         if value is None:
             value = ''
-        html = '''
-        <div id="editor">
-            <textarea name="%(name)s" style="display:none;">%(body)s</textarea>
-        </div>
-        <script type="text/javascript">
-            $(function(){
-                var editor = editormd("editor",{
-                    height: 600,
-                    path: "%(path)s",
-                    htmlDecode: true,
-                    codeFold : true,
-                });
-            });
-        </script>
-    ''' % {
-            'name': name,
-            'body': conditional_escape(force_text(value)),
-            'path': '//cdn.jsdelivr.net/gh/pandao/editor.md@1.5.0/lib/',
-        }
-        return mark_safe(html)
+
+        return mark_safe(render_to_string("editormd.html", {
+            "body": value,
+            "path": self.lib_path,
+        }))
 
     class Media:
         css = {
