68ac3c96a486a196b5e19f6a7210ffba4d0f5206
ngdyj <sadeye@qq.com>
2019-11-24 20:22:13 +0800

    update:widget

--- editormd/widget.py
+++ editormd/widget.py
@@ -1,5 +1,7 @@
 from django.forms import widgets
 from django.utils.safestring import mark_safe
+from django.utils.html import conditional_escape
+from django.utils.encoding import force_text
 
 
 class TextInputMarkdown(widgets.Textarea):
@@ -7,6 +9,8 @@ class TextInputMarkdown(widgets.Textarea):
     please see: https://github.com/pandao/editor.md
     """
     def render(self, name, value, attrs=None, renderer=None):
+        if value is None:
+            value = ''
         html = '''
         <div id="editor">
             <textarea name="%(name)s" style="display:none;">%(body)s</textarea>
@@ -15,13 +19,15 @@ class TextInputMarkdown(widgets.Textarea):
             $(function(){
                 var editor = editormd("editor",{
                     height: 600,
-                    path: "%(path)s"
+                    path: "%(path)s",
+                    htmlDecode: true,
+                    codeFold : true,
                 });
             });
         </script>
     ''' % {
             'name': name,
-            'body': value,
+            'body': conditional_escape(force_text(value)),
             'path': '//cdn.jsdelivr.net/gh/pandao/editor.md@1.5.0/lib/',
         }
         return mark_safe(html)
@@ -31,6 +37,6 @@ class TextInputMarkdown(widgets.Textarea):
             "all": ("//cdn.jsdelivr.net/gh/pandao/editor.md@1.5.0/css/editormd.min.css",)
         }
         js = (
+            "//cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js",
             "//cdn.jsdelivr.net/gh/pandao/editor.md@1.5.0/editormd.min.js",
-            "//cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js"
         )
